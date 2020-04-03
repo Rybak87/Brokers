bc8add0026fe2578d8506477e1ca25d8ecd70cf9
Rybak87 <50304572+Rybak87@users.noreply.github.com>
2020-04-03 18:36:05 +0300

    0.1

---
+++ Consumer.Service/Consumer.cs
@@ -0,0 +1,74 @@
+﻿using Brokers.DAL.Configurations;
+using Brokers.DAL.Consumers;
+using Brokers.DAL.Interfaces;
+using Brokers.DAL.Loggers;
+using Brokers.DAL.Model;
+using log4net;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace Consumer.Service
+{
+    public partial class ConsumerService : ServiceBase
+    {
+        static IMessageConsumer consumer;
+        static ILogger logger = new Log4Net("loggerLog4net");
+        static object locker = new object();
+
+        public ConsumerService()
+        {
+            InitializeComponent();
+
+            try
+            {
+                var settings = (RabbitMQSettings)ConfigurationManager.GetSection("rabbitMQSettings");
+                consumer = new RabbitMQConsumer(settings, logger);
+                //consumer = new KafkaConsumer(logger);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+                return;
+            }
+
+            consumer.StartConsume();
+            //consumer.NewMessage += HandlingMessage;
+
+            Console.Read();
+            consumer.Close();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            consumer.StartConsume();
+        }
+
+        protected override void OnStop()
+        {
+            consumer.Close();
+        }
+
+        private static void HandlingMessage(Message message)
+        {
+            var props = typeof(Message).GetProperties().Select(p => new { name = p.Name, value = p.GetValue(message) });
+
+            lock (locker)
+            {
+                using (var sw = new StreamWriter("C:\\Consumer\\Messages.txt"))
+                {
+
+                    foreach (var item in props)
+                    {
+                        sw.WriteLine(string.Format($"{item.name} = {item.value}"));
+                        sw.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Consumer.Service/InstallerService.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace Consumer.Service
+{
+    [RunInstaller(true)]
+    public partial class InstallerService : System.Configuration.Install.Installer
+    {
+        ServiceInstaller serviceInstaller;
+        ServiceProcessInstaller processInstaller;
+
+        public InstallerService()
+        {
+            InitializeComponent();
+
+            serviceInstaller = new ServiceInstaller();
+            processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "ConsumerService";
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
